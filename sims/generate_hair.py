4018a7cc9c961d731259826134d4fa436573e1b0
Archbishop Uwe A.E. Rosenkranz, MA, DD. <uwe.rosenkranz@gmail.com>
2025-12-11 10:48:53 +0100

    Add hair generation with visualization
    Generate hair silhouette using sine waves and grey streaks.

--- sims/generate_hair.py
+++ sims/generate_hair.py
@@ -1,1 +1,18 @@
+import numpy as np
+import matplotlib.pyplot as plt
 
+height = 1.87
+x = np.linspace(-0.4, 0.4, 600)
+y = np.linspace(height*0.75, height*1.15, 400)
+X, Y = np.meshgrid(x, y)
+
+# Cobain waves + grey streaks
+waves = 0.07 * np.sin(25*X + Y*3) * np.exp(-4*(X)**2)
+grey = 0.3 * np.sin(40*X) * np.exp(-3*(Y-height))
+hair = np.clip(waves + grey + 0.6, 0, 1)
+
+plt.figure(figsize=(8,10))
+plt.contourf(X, Y, hair, levels=30, cmap='Greys', alpha=0.9)
+plt.axis('off')
+plt.savefig("../dist/generated/hair-silhouette.svg", bbox_inches='tight', pad_inches=0, transparent=True)
+plt.close()
