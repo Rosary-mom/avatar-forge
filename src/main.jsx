eed8dbb1489800d95ce00028616eba4d24becfbd
Archbishop Uwe A.E. Rosenkranz, MA, DD. <uwe.rosenkranz@gmail.com>
2025-12-11 11:49:18 +0100

    Fix import statement case sensitivity
    Corrected import statement to match filename case.

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
+import App from './App.jsx'   // ← make sure the A is capital, exactly like the filename
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
