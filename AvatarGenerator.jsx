cd8f8c49cdefba7b5a3d4241cf65c215e4cefb58
Archbishop Uwe A.E. Rosenkranz, MA, DD. <uwe.rosenkranz@gmail.com>
2025-12-12 11:17:57 +0100

    Create AvatarGenerator.jsx

---
+++ AvatarGenerator.jsx
@@ -0,0 +1,14 @@
+// src/App.js (oder ähnlich)
+import React from 'react';
+import AvatarGenerator from './AvatarGenerator';
+
+function App() {
+  return (
+    <div>
+      <h1>Rosary©®™ Avatar Forge – 2030 Templar Fashion</h1>
+      <AvatarGenerator />
+    </div>
+  );
+}
+
+export default App;
